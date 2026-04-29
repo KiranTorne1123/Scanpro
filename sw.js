4a25f8d64dbb481b90bc3dfe5801297db3580d27
Kiran Kailas Torne <167805501+KiranTorne1123@users.noreply.github.com>
2026-04-30 00:55:21 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE = 'scanpro-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'scanpro-v3';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','index.html'])).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+    if (!res || res.status !== 200) return res;
+    caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+    return res;
+  }).catch(() => caches.match('/'))));
 });
